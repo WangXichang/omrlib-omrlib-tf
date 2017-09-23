c8e7c28a7f8287c191cba103cb989a089a5a2a46
wangxichang <wangxichang@126.com>
2017-09-23 13:15:47 +0800

    updata lib1 with xmap, ymap in class omrrecog

--- omr_lib1.py
+++ omr_lib1.py
@@ -16,6 +16,16 @@ class omrrecog():
         self.img = None
         self.result = {'1':[[0,0], [1,1]]}
         self.imdf = None
+        self.xmap = None
+        self.ymap = None
+
+    def test(self):
+        self.get_img()
+        self.convert_df()
+        self.get_xpos()
+        self.get_ypos()
+        self.plotxmap()
+        self.plotymap()
 
     def get_img(self):
         img2 = cv2.imread(imfilepath+r'\{}.jpg'.format('1a3119261913111631103_OMR01'))
@@ -23,17 +33,33 @@ class omrrecog():
 
     def convert_df(self):
         imdf = pd.DataFrame({str(x):self.img[:,x,1] for x in range(self.img.shape[1])})
-        imdf.applymap(lambda x: 255-x)
-        self.imdf = imdf
+        self.imdf = imdf.applymap(lambda x: 255-x)
+        # self.imdf = imdf
 
     def get_xpos(self):
-        u = self.imdf.sum(axis=0)
-        #u['ind'] = u.index
-        #u['ind'] = u['ind'].apply(lambda x: int(x))
+        u = pd.DataFrame(self.imdf.iloc[100:110,:].sum(axis=0))
+        threshold = u.describe().loc['mean', 0]
+        u = u.applymap(lambda x: 1 if x > threshold else 0)
+        u['ind'] = u.index
+        u['ind'] = u['ind'].apply(lambda x: int(x))
+        u = u.sort_values('ind')
         #u.sort_values['ind']
-        plt.plot(u.index, u.values)
+        self.xmap = u
 
     def get_ypos(self):
         v = self.imdf.sum(axis=1)
         v = v.apply(lambda x: 1 if x > v.mean() else 0)
-        plt.plot(v)
+        self.ymap = v
+
+    def show(self):
+        # plt.figure(1)
+        cv2.namedWindow('omr-image', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('omr-image', self.img)
+
+    def plotxmap(self):
+        plt.figure(2)
+        plt.plot(self.xmap.index, self.xmap[0])
+
+    def plotymap(self):
+        plt.figure(3)
+        plt.plot(self.ymap.index, self.ymap.values)
