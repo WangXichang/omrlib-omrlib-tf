1a527cc07fcc393ef0b4bbb1a00e24300b225c1d
wangxichang <wangxichang@126.com>
2017-09-26 23:42:59 +0800

    use numpy replace dataframe, dict reduce time from 6.xx to 0.05 or less

--- omr_lib1.py
+++ omr_lib1.py
@@ -1,123 +1,139 @@
 # *_* utf-8 *-*
 
-#from PIL import Image as im
-import cv2
+
 import numpy as np
 import pandas as pd
 import matplotlib.image as matimg
 import matplotlib.pyplot as plt
+from sklearn import svm
+import time
 
 
 # read omr card image and recognized the omr painting area(points)
 # further give detect function to judge whether the area is painted
-class omrrecog():
+class OmrRecog(object):
     def __init__(self):
         self.img = None
-        self.result = {'1':[[0,0], [1,1]]}
-        self.imdf = None
+        # valid area[rows cope, columns scope] for omr painting points
+        self.omr_row_col = {'row': [0, 12], 'col': [22, 36]}
+        # self.imdf = None
         self.xmap = None
         self.ymap = None
-        self.omrxy = []
+        # self.omrxydict = []
         self.omrxwid = 0
         self.omrywid = 0
         self.omrxnum = 0
         self.omrynum = 0
         self.omriamge = None
         self.omrdict = {}
+        self.omr_svmdata = {}
+        self.omrsvm = None
 
     def test(self):
-        self.get_img()
-        self.get_imageproject()
-        self.get_omr_xyposition()
-        self.get_omrdict()
+        omrfile = 'omrtest.jpg'
+        st = time.clock()
+        self.get_img(omrfile)
+        self.get_xyproj()
+        self.get_omr_xypos()
+        self.get_omr_xyarea_dict()
+        # self.get_omrimage()
+        # self.get_svm()
+        print(f'consume {time.clock()-st}')
 
-    def get_img(self):
-        # img = cv2.imread('omrtest0.jpg')
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # imdf = pd.DataFrame({str(x):self.img[:,x] for x in range(self.img.shape[1])})
-        self.img = matimg.imread('omrtest0.jpg')
-        imdf = pd.DataFrame(self.img)
-        self.imdf = imdf.applymap(lambda x: 255-x)
+    def get_img(self, imfile):
+        self.img = 255 - matimg.imread(imfile)
 
-    def get_imageproject(self):
-        h, w = self.img.shape
-        omr_clip_h = 50
+    def get_xyproj(self):
+        rowNum, colNum = self.img.shape
+        omr_clip_row = 50     # columns mark points at bottom of page
+        omr_clip_col = 80     # rows mark points at right of page
         # project to X-axis
-        u = pd.DataFrame(self.imdf.iloc[h - omr_clip_h:h, :].sum(axis=0))
-        threshold = u.describe().loc['mean', 0]
-        u = u.applymap(lambda x: 1 if x > threshold else 0)
-        u['ind'] = u.index
-        u['ind'] = u['ind'].apply(lambda x: int(x))
-        u = u.sort_values('ind')
-        self.xmap = u[[0]]
+        self.xmap = [self.img[rowNum - omr_clip_row:rowNum-1, x].sum() for x in range(colNum)]
+        hmean = sum(self.xmap) / len(self.xmap)
+        self.xmap = [1 if x > hmean else 0 for x in self.xmap]
         # project to Y-axis
-        v = pd.DataFrame(self.imdf.sum(axis=1))
-        v[0] = v[0].apply(lambda x: 1 if x > v[0].mean() else 0)
-        self.ymap = v
+        self.ymap = [self.img[y, colNum - omr_clip_col:colNum-1].sum() for y in range(rowNum)]
+        wmean = sum(self.ymap) / len(self.ymap)
+        self.ymap = [1 if x > wmean else 0 for x in self.ymap]
 
-    def get_omr_xyposition(self):
-        xlist = []
-        xlist2 = []
-        ylist = []
-        ylist2 = []
+    def get_omr_xypos(self):
+        x_start = []
+        x_end = []
+        y_start = []
+        y_end = []
+        # model to detect start or end pos of mark points
         m = np.array([-1, 1, 1])
         m2 = np.array([1, 1, -1])
         for x in range(1, len(self.xmap)-1):
-            if sum(self.xmap.loc[x-1:x+1, 0] * m) == 2:
-                xlist = xlist +[x]
-            if sum(self.xmap.loc[x-1:x+1, 0] * m2) == 2:
-                xlist2 = xlist2 + [x]
+            if np.dot(self.xmap[x-1:x+2], m) == 2:
+                x_start = x_start + [x]
+            if np.dot(self.xmap[x - 1:x + 2], m2) == 2:
+                x_end = x_end + [x]
         for y in range(1, len(self.ymap)-1):
-            if sum(self.ymap.loc[y-1:y+1, 0] * m) == 2:
-                ylist = ylist +[y]
-            if sum(self.ymap.loc[y-1:y+1, 0] * m2) == 2:
-                ylist2 = ylist2 +[y]
-        if len(xlist) != len(xlist2):
+            if np.dot(self.ymap[y-1:y+2], m) == 2:
+                y_start = y_start + [y]
+            if np.dot(self.ymap[y-1:y+2], m2) == 2:
+                y_end = y_end + [y]
+        # check x-start/x-end pairs
+        if len(x_start) != len(x_end):
             print('check x wave error!')
         else:
-            self.omrxnum = len(xlist)
-            self.omrxwid = round(sum([round(abs(x1 - x2)) for x1, x2 in zip(xlist, xlist2)]) / self.omrxnum)
-        if len(ylist) != len(ylist2):
+            self.omrxnum = len(x_start)
+        # check y-start/y-end pairs
+        if len(y_start) != len(y_end):
             print('check x wave error!')
         else:
-            self.omrynum = len(ylist)
-            self.omrxwid = round(sum([abs(x1 - x2) for x1, x2 in zip(ylist, ylist2)]) / self.omrynum)
-        self.omrxy = [xlist, xlist2, ylist, ylist2]
+            self.omrynum = len(y_start)
+        # return x-pos, y-pos in list
+        # self.omrxydict = {'x_start':x_start, 'x_end':x_end, 'y_start':y_start, 'y_end':y_end}
+        self.omrxypos = np.array([x_start, x_end, y_start, y_end])
 
-    def get_omrdict(self):
-        # omrimage = 255 - np.zeros([self.omrxnum, self.omrynum])
-        omrimage = self.imdf.applymap(lambda x:0)
-        for y in range(self.omrynum - 1):
-            for x in range(self.omrxnum - 1):
-                self.omrdict[(y,x)] = self.imdf.iloc[self.omrxy[2][y]:self.omrxy[3][y], \
-                                                     self.omrxy[0][x]:self.omrxy[1][x]]
-                omrimage.iloc[self.omrxy[2][y]:self.omrxy[3][y], self.omrxy[0][x]:self.omrxy[1][x]] = \
-                    self.omrdict[(y,x)]
+    def get_omr_xyarea_dict(self):
+        # cut area for painting points
+        for y in range(self.omr_row_col['row'][0], self.omr_row_col['row'][1]):
+            for x in range(self.omr_row_col['col'][0], self.omr_row_col['col'][1]):
+                self.omrdict[(y,x)] = self.img[self.omrxypos[2][y]:self.omrxypos[3][y]+1,
+                                               self.omrxypos[0][x]:self.omrxypos[1][x]+1]
+
+    def get_omrimage(self):
+        # create a blackgroud model for omr display image
+        omrimage = np.zeros(self.img.shape)
+        for y in range(self.omr_row_col['row'][0], self.omr_row_col['row'][1]):
+            for x in range(self.omr_row_col['col'][0], self.omr_row_col['col'][1]):
+                omrimage[self.omrxypos[2][y]:self.omrxypos[3][y]+1,
+                         self.omrxypos[0][x]:self.omrxypos[1][x]+1] = self.omrdict[(y,x)]
         self.omriamge = omrimage
 
+    # test use svm in sklearn
+    def get_svm(self):
+        self.omr_svmdata = {'data':[], 'label':[]}
+        for i in range(self.omr_row_col['row'][0],self.omr_row_col['row'][1]):
+            for j in range(self.omr_row_col['col'][0],self.omr_row_col['col'][1]):
+                painted_mean = self.omrdict[(i,j)].mean().mean()
+                painted_std0 = self.omrdict[(i,j)].sum(axis=0).std()
+                # painted_std1 = self.omrdict[(i,j)].sum(axis=1).std()
+                # self.omr_svmdata['data'].append([painted_mean, painted_std0, painted_std1])
+                self.omr_svmdata['data'].append([painted_mean, painted_std0])
+                if painted_mean >= 100:
+                    # print(f'painted points: {(i, j)}')
+                    self.omr_svmdata['label'].append(1)
+                else:
+                    self.omr_svmdata['label'].append(0)
+        clf = svm.LinearSVC()
+        clf.fit(self.omr_svmdata['data'], self.omr_svmdata['label'])
+        self.omrsvm = clf
+
     def show_rawimage(self):
-        # cv2 mode
-        # cv2.namedWindow('omr-image', cv2.WINDOW_AUTOSIZE)
-        #  cv2.imshow('omr-image', self.img)
-        # plt.figure(1)
-        # matplotlib mode
         plt.imshow(self.img)
 
     def plot_xmap(self):
         plt.figure(2)
-        plt.plot(self.xmap.index, self.xmap[0])
+        plt.plot(self.xmap)
 
     def plot_ymap(self):
         plt.figure(3)
-        plt.plot(self.ymap.index, self.ymap.values)
+        plt.plot(self.ymap)
 
     def plot_omrimage(self):
         plt.figure('recognized - omr - region')
         plt.imshow(self.omriamge)
-
-    def get_painted(self):
-        for i in range(5):
-            for j in range(1,30):
-                paintedlevel = self.omrdict[(i,j)].mean().mean()
-                if paintedlevel >= 100:
-                    print(f'painted points: {(i, j)}')
