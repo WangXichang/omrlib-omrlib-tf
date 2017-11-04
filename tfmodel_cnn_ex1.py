9f8ec679755bd3b655eac6f1946072b68f891349
wangxichang <wangxichang@126.com>
2017-11-04 10:33:47 +0800

    cnn_ex1 do not work with error: feed cannot need tensor object

--- tfmodel_cnn_ex1.py
+++ tfmodel_cnn_ex1.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
+# import numpy as np
+# from tensorflow.examples.tutorials.mnist import input_data
 
 
 def weight_variable(shape):
@@ -20,8 +20,8 @@ def max_pool_2x2(x):
 
 
 def evaluate(y, y_):
-    y = tf.arg_max(input=y, dimension=1)
-    y_ = tf.arg_max(input=y_, dimension=1)
+    y = tf.argmax(input=y)  # , dimension=1)  # tf.arg_max(input=y, dimension=1)
+    y_ = tf.argmax(input=y_)  # , dimension=1)  #tf.arg_max(input=y_, dimension=1)
     return tf.reduce_mean(input_tensor=tf.cast(tf.equal(y, y_), tf.float32))
 
 
@@ -37,9 +37,9 @@ def read_and_decode(filename, image_reshape=(10, 15, 1)):
                                        })
     img = tf.decode_raw(features['image'], tf.uint8)
     img = tf.reshape(img, image_reshape)  # [10, 15, 1])
-    # img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
-    # label = tf.cast(features['label'], tf.int32)
-    lab = features['label']
+    img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+    lab = tf.cast(features['label'], tf.int32)
+    # lab = features['label']
     return img, lab
 
 
@@ -48,15 +48,18 @@ def test_cnn(batch_size=30, lr=0.0001, num_iter=20000):
     # dataset = input_data.read_data_sets(train_dir='MNIST_data/', one_hot=True)
     # dataset = test_data()
 
-    image_test, label_test = read_and_decode("tf_data.tfrecords", [10, 15, 1])
+    # image_test, label_test = read_and_decode("tf_data.tfrecords")
     image, label = read_and_decode("tf_card_2.tfrecords", [10, 15, 1])
+    image_batch, label_batch = tf.train.batch([image, label],
+                                              batch_size=batch_size,
+                                              capacity= 300)
     # 使用shuffle_batch可以随机打乱输入
-    image_batch, label_batch = tf.train.shuffle_batch([image, label],
-                                                      batch_size=batch_size,
-                                                      capacity=2000,
-                                                      min_after_dequeue=1000)
+    # image_batch, label_batch = tf.train.shuffle_batch([image, label],
+    #                                                   batch_size=batch_size,
+    #                                                   capacity=200,
+    #                                                  min_after_dequeue=100)
     x = tf.placeholder(dtype=tf.float32, shape=[None, 150], name='images')
-    y = tf.placeholder(dtype=tf.float32, shape=[None, 2], name='labels')
+    y = tf.placeholder(dtype=tf.int32, shape=[None, 2], name='labels')
     # 后面的卷积操作输入参数必须为‘float32’或者‘float64’
 
     w_conv1 = weight_variable(shape=[5, 5, 1, 32])
@@ -82,29 +85,31 @@ def test_cnn(batch_size=30, lr=0.0001, num_iter=20000):
     b_softmax = bias_variable(shape=[2])
     softmax_in = tf.matmul(full_connected_out, w_softmax) + b_softmax
     softmax_out = tf.nn.softmax(logits=softmax_in, name='softmax_layer')
-    Loss = tf.nn.softmax_cross_entropy_with_logits(logits=softmax_in, labels=y)
-    Step_train = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss=Loss)
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits=softmax_in, labels=y)
+    step_train = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss=loss)
     # 在测试数据集上评估算法的准确率
     accuracy = evaluate(y, softmax_out)
-    initialized_variables = tf.initialize_all_variables()
+    # initialized_variables = tf.initialize_all_variables()
+    initialized_variables = tf.global_variables_initializer()
 
     print('Start to train the convolutional neural network......')
     sess = tf.Session()
     sess.run(fetches=initialized_variables)
     for iter in range(num_iter):
+        # image, label = read_and_decode("tf_card_2.tfrecords", [10, 15, 1])
         # batch = dataset.train.next_batch(batch_size=batch_size)
         # sess.run(fetches=Step_train, feed_dict={x: batch[0], y: batch[1], dropout_prob: 0.5})
-        sess.run(fetches=Step_train, feed_dict={x: image_batch, y: label_batch, dropout_prob: 0.5})
+        sess.run(fetches=step_train, feed_dict={x: image_batch, y: label_batch, dropout_prob: 0.5})
         if (iter+1) % 100 == 0:  # 计算在当前训练块上的准确率
-            Accuracy = sess.run(fetches=accuracy, feed_dict={x: image_batch,
+            accuracy_id = sess.run(fetches=accuracy, feed_dict={x: image_batch,
                                                              y: label_batch, dropout_prob: 1})
-            print('Iter num %d ,the train accuracy is %.3f' % (iter+1, Accuracy))
+            print('Iter num %d ,the train accuracy is %.3f' % (iter+1, accuracy_id))
 
-    Accuracy = sess.run(fetches=accuracy, feed_dict={x: image_test,
-                                                     y: label_test,
-                                                     dropout_prob: 1})
+    #Accuracy = sess.run(fetches=accuracy, feed_dict={x: image_test,
+    #                                                 y: label_test,
+    #                                                 dropout_prob: 1})
     sess.close()
-    print('Train process finished, the best accuracy is %.3f' % Accuracy)
+    # print('Train process finished, the best accuracy is %.3f' % Accuracy)
 
 
 if __name__ == '__main__':
