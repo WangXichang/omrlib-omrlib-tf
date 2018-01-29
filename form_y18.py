24244d903a18a6478406cd46554bc3ce1d1ae14e
wangxichang <wangxichang@126.com>
2018-01-29 15:04:28 +0800

    form_y18

--- form_y18.py
+++ form_y18.py
@@ -5,51 +5,7 @@ import glob
 import omr_lib1 as ol1
 
 
-def form_101():
-    omr_location = [r"d:/work/data/y18/101/*"]
-    omr_image_list = []
-    for loc in omr_location:
-        loc1 = glob.glob(loc)
-        for ds in loc1:
-            omr_image_list = omr_image_list + \
-                glob.glob(ds + r'\*.jpg')
-    group = {
-        j+6*h: [(41+j, 4+6*h), 4, 'H', 'ABCD', 'S'] for j in range(1, 7)
-        for h in range(0, 5)
-        }
-    group.update({
-        j + 30: [(41+j, 34), 4, 'H', 'ABCD', 'S'] for j in range(1, 4)
-        })
-    group.update({
-        j+33: [(12, 23+j), 10, 'V', '0123456789', 'S'] for j in range(1, 16)
-        })
-    card_form = {
-        'len': omr_image_list.__len__(),
-        'image_file_list': omr_image_list,
-        'check_vertical_mark_from_right': False,
-        'mark_format': {
-            'mark_col_number': 38,
-            'mark_row_number': 50,
-            'mark_valid_area_col_start': 4,
-            'mark_valid_area_col_end': 38,
-            'mark_valid_area_row_start': 11,
-            'mark_valid_area_row_end': 49,
-            'mark_location_row_no': 50,
-            'mark_location_col_no': 1
-        },
-        'group_format': group,
-        'image_clip': {
-            'do_clip': False,
-            'x_start': 0,
-            'x_end': -1,
-            'y_start': 0,
-            'y_end': -1
-        }
-    }
-    return card_form
-
-
-def new_form101():
+def form101():
     omrform = ol1.OmrForm()
     omrform.set_image_clip(
        clip_x_start=1,
@@ -94,60 +50,7 @@ def new_form101():
     return omrform
 
 
-def form_109():
-    omr_location = [r"d:/work/data/y18/109/*"]
-    omr_image_list = []
-    for loc in omr_location:
-        loc1 = glob.glob(loc)
-        for ds in loc1:
-            omr_image_list = omr_image_list + \
-                glob.glob(ds + r'\*.jpg')
-    group = ({
-        j: [(14, 23+j), 10, 'V', '0123456789', 'S'] for j in range(1, 16)
-        })
-    group.update({
-        j+5*h+15: [(42+j+h*6, 5), 5, 'H', 'ABCDE', 'S'] for j in range(1, 6)
-        for h in range(0, 3)
-        })
-    group.update({
-        j+15+5*h+15: [(42+j+h*6, 14), 5, 'H', 'ABCDE', 'S'] for j in range(1, 6)
-        for h in range(0, 3)
-        })
-    group.update({
-        j+15+5*h+30: [(42+j+h*6, 23), 5, 'H', 'ABCDE', 'S'] for j in range(1, 6)
-        for h in range(0, 3)
-        })
-    group.update({
-        j+15+5*h+45: [(42+j+h*6, 32), 5, 'H', 'ABCDE', 'S'] for j in range(1, 6)
-        for h in range(0, 2)
-        })
-    card_form = {
-        'len': omr_image_list.__len__(),
-        'image_file_list': omr_image_list,
-        'check_vertical_mark_from_right': False,
-        'mark_format': {
-            'mark_col_number': 38,
-            'mark_row_number': 61,
-            'mark_valid_area_col_start': 4,
-            'mark_valid_area_col_end': 38,
-            'mark_valid_area_row_start': 14,
-            'mark_valid_area_row_end': 60,
-            'mark_location_row_no': 61,
-            'mark_location_col_no': 1
-        },
-        'group_format': group,
-        'image_clip': {
-            'do_clip': False,
-            'x_start': 0,
-            'x_end': -1,
-            'y_start': 0,
-            'y_end': -1
-        }
-    }
-    return card_form
-
-
-def new_form109():
+def form109():
     omrform = ol1.OmrForm()
     omrform.set_image_clip(
        clip_x_start=1,
@@ -156,6 +59,9 @@ def new_form109():
        clip_y_end=-1,
        do_clip=False)
     omrform.set_file_list(path='d:/work/data/y18/109/', substr='jpg')
+    # check mark setting
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     omrform.set_mark_format(
         row_number=61,
         col_number=38,
@@ -200,69 +106,6 @@ def new_form109():
 
 
 def form_201():
-    omrform = ol1.OmrForm()
-
-    omrform.set_file_list(path='d:/work/data/y18/201/',
-                          substr='jpg')
-
-    omrform.set_mark_format(row_number=55,
-                            col_number=38,
-                            valid_area_row_start=14,
-                            valid_area_row_end=54,
-                            valid_area_col_start=4,
-                            valid_area_col_end=38,
-                            location_row_no=55,
-                            location_col_no=1
-                            )
-
-    omrform.set_image_clip(clip_x_start=0,
-                           clip_x_end=-1,
-                           clip_y_start=0,
-                           clip_y_end=-1,
-                           do_clip=False)
-
-    for g in range(1, 16):  # 1-15
-        omrform.set_group(group=g,
-                          coord=(14, 23+g),
-                          code_dire='V',
-                          code_set='0123456789',
-                          code_mode='S')
-    for g in range(1, 11):  # 16-25
-        omrform.set_group(group=g+15,
-                          coord=(43+g, 4),
-                          code_dire='H',
-                          code_set='ABCD',
-                          code_mode='S')
-    for g in range(1, 11):  # 26-35
-        omrform.set_group(group=g+25,
-                          coord=(55-12+g, 10),
-                          code_dire='H',
-                          code_set='ABCD',
-                          code_mode='S')
-    for g in range(1, 11):  # 36-45
-        omrform.set_group(group=g+35,
-                          coord=(55-12+g, 18),
-                          code_dire='H',
-                          code_set='ABCD',
-                          code_mode='S')
-    for g in range(1, 11):  # 46-55
-        omrform.set_group(group=g+45,
-                          coord=(55-12+g, 24),
-                          code_dire='H',
-                          code_set='ABCD',
-                          code_mode='S')
-    for g in range(1, 6):  # 56-60
-        omrform.set_group(group=g+55,
-                          coord=(55-12+g, 31),
-                          code_dire='H',
-                          code_set='ABCDEFG',
-                          code_mode='S')
-
-    omrform.get_form()
-    return omrform
-
-
-def form_201n():
     omrform = ol1.OmrForm()
 
     omrform.set_image_clip(
@@ -320,59 +163,56 @@ def form_201n():
 def form_202():
     omrform = ol1.OmrForm()
 
-    omrform.set_file_list(path='d:/work/data/y18/202/',
-                          substr='jpg')
+    omrform.set_file_list(
+        path='d:/work/data/y18/202/',
+        substr='jpg')
 
-    # omrform.set_check_mark_h_from_bottom(True)
-    # omrform.set_check_mark_v_from_right(False)
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
 
-    omrform.set_mark_format(row_number=51,
-                            col_number=38,
-                            valid_area_row_start=13,
-                            valid_area_row_end=50,
-                            valid_area_col_start=5,
-                            valid_area_col_end=38,
-                            location_row_no=51,
-                            location_col_no=1
-                            )
+    omrform.set_mark_format(
+        row_number=51,
+        col_number=38,
+        valid_area_row_start=13,
+        valid_area_row_end=50,
+        valid_area_col_start=5,
+        valid_area_col_end=38,
+        location_row_no=51,
+        location_col_no=1
+        )
 
-    omrform.set_image_clip(clip_x_start=0,
-                           clip_x_end=-1,
-                           clip_y_start=0,
-                           clip_y_end=-1,
-                           do_clip=False)
+    omrform.set_image_clip(
+        clip_x_start=0,
+        clip_x_end=-1,
+        clip_y_start=0,
+        clip_y_end=-1,
+        do_clip=False)
 
-    for g in range(1, 16):  # 1-15
-        omrform.set_group(group=g,
-                          coord=(13, 23+g),
-                          code_dire='V',
-                          code_set='0123456789',
-                          code_mode='S')
-    for g in range(1, 11):  # 16-25
-        omrform.set_group(group=g+15,
-                          coord=(39+g, 5),
-                          code_dire='H',
-                          code_set='ABCD',
-                          code_mode='S')
-    for g in range(1, 11):  # 26-35
-        omrform.set_group(group=g+25,
-                          coord=(39+g, 14),
-                          code_dire='H',
-                          code_set='ABCD',
-                          code_mode='S')
-    for g in range(1, 11):  # 36-45
-        omrform.set_group(group=g+35,
-                          coord=(39+g, 23),
-                          code_dire='H',
-                          code_set='ABCD',
-                          code_mode='S')
-    for g in range(1, 11):  # 46-55
-        omrform.set_group(group=g+45,
-                          coord=(39+g, 32),
-                          code_dire='H',
-                          code_set='ABCD',
-                          code_mode='S')
-    omrform.get_form()
+    # define area
+    omrform.set_area(
+        area_group_min_max=(1, 15),  # area group from min=a to max=b (a, b)
+        area_location_leftcol_toprow=(13, 24),  # area location left_top = (row, col)
+        area_direction='h',         # area direction V:top to bottom, H:left to right
+        code_dire='v',              # group direction from left to right
+        code_set='0123456789',      # group code set for encoding
+        code_mode='S'               # 'M':multi_choice, 'S':single_choice
+    )
+
+    # define cluster
+    # group for each area: (min_no, max_no)
+    cluster_group = [(101 + i * 10, 110 + i * 10) for i in range(4)]
+    # location for each area: (left_col, top_row)
+    cluster_coord = [(40, 5+j*9) for j in range(4)]
+    for group_scope, loc_coord in zip(cluster_group, cluster_coord):
+        omrform.set_area(
+            area_group_min_max=group_scope,  # area group from min=a to max=b (a, b)
+            area_location_leftcol_toprow=loc_coord,  # area location left_top = (row, col)
+            area_direction='v',  # area direction V:top to bottom, H:left to right
+            code_dire='h',      # group direction from left to right
+            code_set='ABCD',    # group code set for encoding
+            code_mode='S'       # 'M':multi_choice, 'S':single_choice
+        )
+
     return omrform
 
 
@@ -380,6 +220,13 @@ def form_203():
     omrform = ol1.OmrForm()
     omrform.set_file_list(path='d:/work/data/y18/203/',
                           substr='jpg')
+    omrform.set_image_clip(clip_x_start=0,
+                           clip_x_end=-1,
+                           clip_y_start=0,
+                           clip_y_end=-1,
+                           do_clip=False)
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     omrform.set_mark_format(row_number=51,
                             col_number=38,
                             valid_area_row_start=13,
@@ -406,12 +253,6 @@ def form_203():
             code_dire='h',
             code_set='ABCD',
             code_mode='S')
-
-    omrform.set_image_clip(clip_x_start=0,
-                           clip_x_end=-1,
-                           clip_y_start=0,
-                           clip_y_end=-1,
-                           do_clip=False)
     return omrform
 
 
@@ -420,6 +261,9 @@ def form_204():
 
     omrform.set_file_list(path='d:/work/data/y18/204/',
                           substr='jpg')
+
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
 
     omrform.set_mark_format(row_number=55,
                             col_number=38,
@@ -460,6 +304,9 @@ def form_311():
 
     omrform.set_file_list(path='d:/work/data/y18/311/',
                           substr='jpg')
+
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
 
     omrform.set_mark_format(
         row_number=50,
@@ -504,7 +351,10 @@ def form_314():
        clip_y_start=1,
        clip_y_end=-1,
        do_clip=False)
-    omrform.set_file_list(path='d:/work/data/y18/314/723084/', substr='jpg')
+    omrform.set_file_list(path='d:/work/data/y18/314/723084/',
+                          substr='jpg')
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     omrform.set_mark_format(
         row_number=44,
         col_number=26,
@@ -547,7 +397,10 @@ def form_315():
        clip_y_start=1,
        clip_y_end=-1,
        do_clip=False)
-    omrform.set_file_list(path='d:/work/data/y18/315/738111/', substr='jpg')
+    omrform.set_file_list(path='d:/work/data/y18/315/738111/',
+                          substr='jpg')
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     omrform.set_mark_format(
         row_number=56,
         col_number=26,
@@ -589,7 +442,10 @@ def form_397():
        clip_y_start=1,
        clip_y_end=-1,
        do_clip=False)
-    omrform.set_file_list(path='d:/work/data/y18/397', substr='jpg')
+    omrform.set_file_list(path='d:/work/data/y18/397',
+                          substr='jpg')
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     omrform.set_mark_format(
         row_number=45,
         col_number=38,
@@ -631,7 +487,10 @@ def form_398():
        clip_y_start=1,
        clip_y_end=-1,
        do_clip=False)
-    omrform.set_file_list(path='d:/work/data/y18/398', substr='jpg')
+    omrform.set_file_list(path='d:/work/data/y18/398',
+                          substr='jpg')
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     omrform.set_mark_format(
         row_number=50,
         col_number=38,
@@ -672,7 +531,10 @@ def form_408():
        clip_y_start=1,
        clip_y_end=-1,
        do_clip=False)
-    omrform.set_file_list(path='d:/work/data/y18/408', substr='jpg')
+    omrform.set_file_list(path='d:/work/data/y18/408',
+                          substr='jpg')
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     omrform.set_mark_format(
         row_number=50,
         col_number=38,
@@ -713,7 +575,10 @@ def form_414_omr01():
        clip_y_start=1,
        clip_y_end=-1,
        do_clip=False)
-    omrform.set_file_list(path='d:/work/data/y18/414', substr='jpg')
+    omrform.set_file_list(path='d:/work/data/y18/414',
+                          substr='jpg')
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     for f in omrform.form['image_file_list']:
         if 'Omr02' in f:
             omrform.form['image_file_list'].remove(f)
@@ -757,7 +622,10 @@ def form_414_omr02():
        clip_y_start=1,
        clip_y_end=-1,
        do_clip=False)
-    omrform.set_file_list(path='d:/work/data/y18/414', substr='Omr02.jpg')
+    omrform.set_file_list(path='d:/work/data/y18/414',
+                          substr='Omr02.jpg')
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     omrform.set_mark_format(
         row_number=45,
         col_number=38,
@@ -798,7 +666,10 @@ def form_498():
        clip_y_start=1,
        clip_y_end=-1,
        do_clip=False)
-    omrform.set_file_list(path='d:/work/data/y18/498', substr='jpg')
+    omrform.set_file_list(path='d:/work/data/y18/498',
+                          substr='jpg')
+    omrform.set_check_mark_from_bottom(True)
+    omrform.set_check_mark_from_right(False)
     omrform.set_mark_format(
         row_number=50,
         col_number=38,
@@ -833,7 +704,8 @@ def form_498():
 
 def form_497():
     f = form_397()
-    f.set_file_list(path='d:/work/data/y18/497', substr='01.jpg')
+    f.set_file_list(path='d:/work/data/y18/497',
+                    substr='01.jpg')
     for gn in range(121, 131):
         f.set_group_mode(gn, 'M')
     return f
