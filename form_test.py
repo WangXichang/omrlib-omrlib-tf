a62a76e02ba2fb758f232dcef8d3babd7b080abb
wangxichang <wangxichang@126.com>
2018-02-09 13:23:18 +0800

    form_test with new former to form22

--- form_test.py
+++ form_test.py
@@ -63,47 +63,6 @@ def form_1():
 
 
 def form_21():
-    former = omrlib.FormBuilder()
-    former.set_image_clip(
-       clip_x_start=1,
-       clip_x_end=-40,
-       clip_y_start=1,
-       clip_y_end=-1,
-       do_clip=True)
-
-    former.set_file_list(path='d:/work/data/omrimage2/',
-                         substr='OMR01.jpg')
-
-    former.set_check_mark_from_bottom(True)
-    former.set_check_mark_from_right(True)
-
-    former.set_mark_format(
-        row_number=6,
-        col_number=31,
-        valid_area_row_start=1,
-        valid_area_row_end=5,
-        valid_area_col_start=1,
-        valid_area_col_end=30,
-        location_row_no=6,
-        location_col_no=31
-        )
-    # define area_cluster, including multi group_areas
-    # group2 = {i + j * 5: [(i, 2 + j * 6), 4, 'H', 'ABCD', 'S'] for i in range(1, 6)
-    cluster_area_group = [(j*5+1, j*5+5) for j in range(5)]   # group no list: (min, max)
-    cluster_area_coord = [(1, 2+j*6) for j in range(5)]
-    for group_min2max, area_coord in zip(cluster_area_group, cluster_area_coord):
-        former.set_area(
-            area_group_min_max=group_min2max,
-            area_location_leftcol_toprow=area_coord,
-            area_direction='v',       # area from top down to bottom
-            group_direction='h',      # group direction from left to right
-            group_code='ABCD',    # group code for painting point
-            group_mode='S'       # if group_min2max[0] in range(, ) else 'M'
-        )
-    return former
-
-
-def form_22():
 
     former = omrlib.FormBuilder()
 
@@ -145,4 +104,68 @@ def form_22():
             group_code='0123456789',    # group code for painting point
             group_mode='D'              # if group_min2max[0] in range(, ) else 'M'
         )
+    return former
+
+
+def form_22():
+    
+    # define former
+    former = omrlib.FormBuilder()
+    
+    # define image file
+    former.set_file_list(
+        path='d:/work/data/omrimage2/', 
+        substr='OMR01.jpg'    # assign substr in filename+pathstr
+        )
+    
+    # define mark location for checking mark 
+    former.set_check_mark_from_bottom(True)
+    former.set_check_mark_from_right(True)
+    
+    # define mark format: row/column number, valid area, location
+    former.set_mark_format(
+        row_number=6,
+        col_number=31,
+        valid_area_row_start=1,
+        valid_area_row_end=5,
+        valid_area_col_start=1,
+        valid_area_col_end=30,
+        location_row_no=6,
+        location_col_no=31
+        )
+    
+    # define cluster
+    # _group: (min_no, max_no), _coord: (left_col, top_row)
+    cluster_group = [(1, 5), (6, 10), (11, 15), (16, 20), (21, 25)]
+    cluster_coord = [(1, 1), (1, 8),  (1, 14), (1, 20), (1, 26)]
+    for gno, loc in zip(cluster_group, cluster_coord):
+        former.set_area(
+            area_group_min_max=gno,                    # area group from min=a to max=b (a, b)
+            area_location_leftcol_toprow=loc,          # area location left_top = (row, col)
+            area_direction='v',                        # area direction V:top to bottom, H:left to right
+            group_direction='h',     # group direction 'V','v': up to down, 'H','h': left to right
+            group_code='ABCD',       # group code for painting block
+            group_mode='S'           # group mode 'M': multi_choice, 'S': single_choice
+            )
+    
+    # define image clip setting
+    former.set_clip(
+        do_clip=False,
+        clip_left=0,
+        clip_right=0,
+        clip_top=0,
+        clip_bottom=0
+        )
+                
+    # define model parameters
+    former.set_model_para(
+        valid_painting_gray_threshold=35,
+        valid_peak_min_width=3,
+        valid_peak_min_max_width_ratio=5,
+        detect_mark_vertical_window=20,
+        detect_mark_horizon_window=20,
+        detect_mark_step_length=5,
+        detect_mark_max_count=100
+        )
+
     return former
