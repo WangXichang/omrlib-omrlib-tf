5884cea0ff39069626698a29ab32f6511f9a9614
wangxichang <wangxichang@126.com>
2018-04-02 08:08:24 +0800

    refine weebar.bar128, test2

--- test_bardecode.py
+++ test_bardecode.py
@@ -14,13 +14,13 @@ import weebar as wb
 f8 = ts.form_8()
 
 
-def test(th_shift=70, scan_scope=12, filenum=100):
-    file_list = f8.file_list[0:filenum]
+def test(form, th_shift=70, scan_scope=12, filenum=100):
+    file_list = form.file_list[0:filenum]
     st = time.time()
     bar=wb.Barcoder()
     bar.image_threshold_shift = th_shift
     bar.image_scan_scope = scan_scope
-    bar.set_image_clip(clip_bottom=600, clip_right=700)
+    bar.set_image_clip(clip_bottom=900, clip_right=800, clip_left=500)
     bar.set_image_files(file_list)
     bar.get_barcode('128')
     print('total time:', time.time()-st)
