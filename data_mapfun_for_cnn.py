eb3f587dcd342cb4fccc394d6f82dfb16bfeed67
wangxichang <wangxichang@126.com>
2018-06-08 07:48:10 +0800

    mapfun

--- data_mapfun_for_cnn.py
+++ data_mapfun_for_cnn.py
@@ -4,11 +4,12 @@ import openomr as opo
 import pandas as pd
 import form_test as ft
 
+
 def get_data(former):
     label_list = []
     data_list = []
-    for f in former.file_list:
-        print(f)
+    for i, f in enumerate(former.file_list):
+        print(i, f)
         mt = opo.read_test(former, f, display=False)
         for k in mt.pos_prj_log:
             if k in mt.pos_valid_prj_log:
@@ -16,6 +17,15 @@ def get_data(former):
             else:
                 label_list.append(0)
             dmax = max(mt.pos_prj_log[k])
-            data_list.append([x/dmax for x in mt.pos_prj_log[k]])
+            dmin = min(mt.pos_prj_log[k])
+            dgap = dmax - dmin
+            dgap = 1 if dgap == 0 else dgap
+            dstr = ''.join([float_str((x-dmin)/(dgap), 1, 6)+'/' for x in mt.pos_prj_log[k]])
+            data_list.append(dstr)
     dfm = pd.DataFrame({'label': label_list, 'data': data_list})
     return dfm
+
+
+def float_str(x, d1, d2):
+    fs = '{:'+str(d1+d2+1)+'.'+str(d2)+'f}'
+    return fs.format(x)
